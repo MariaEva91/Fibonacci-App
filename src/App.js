77fcd9562b1e64b4cb681a9ee508a9459742b6c2
MariaEva91 <mariaevapizzorno@gmail.com>
2019-02-13 19:00:01 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      input: '',
+      fibonacci: ''
+
+
+    }
+    this.handleInputOnChange = this.handleInputOnChange.bind(this);
+    this.anterior = this.anterior.bind(this);
+    this.error = this.error.bind(this);
+    this.fibonacci = this.fibonacci.bind(this);
+    this.siguiente = this.siguiente.bind(this);
+  }
+
+  //function input value
+
+  handleInputOnChange(event) {
+    this.setState({
+      input: event.target.value
+    })
+  }
+
+  //operations
+
+  error() {
+    if (this.state.input < 0) {
+      alert('No existe. Debe ingresar un número mayor a 0');
+    }
+  }
+  // fibonacci serie. This function calculate the next number
+
+  fibonacci() {
+    let fibonacci = [0, 1]
+
+    for (let i = 1; i < 100; i++) {
+      let suma = fibonacci[i] + fibonacci[i - 1];
+      fibonacci.push(suma);
+    }
+    this.setState({
+      fibonacci: fibonacci
+    })
+    console.log(fibonacci);
+  }
+
+  //function previous number
+  anterior() {
+
+  }
+
+  //function next number
+
+  siguiente() {
+
+  }
+
+
+
   render() {
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <p>Fibonacci App</p>
+        <div> <input placeholder="Introduzca un número"
+          className="input"
+          type="number"
+          onChange={this.handleInputOnChange}></input></div>
+
+        <button
+          onClick={this.anterior}
+          onClick={this.error}>Anterior</button>
+        <button
+          onClick={this.fibonacci} onClick={this.siguiente}>Siguiente</button>
+        <div>{this.state.input}</div>
       </div>
     );
   }
